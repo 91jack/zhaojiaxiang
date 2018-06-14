df71b0ae250010bdfaacd0963d46f88d851d9adf
91jack <2764132626@qq.com>
2018-06-14 17:29:10 +0800

    1
    1

--- static/js/common.js
+++ static/js/common.js
@@ -3,21 +3,16 @@
   document.addEventListener('DOMContentLoaded', function () {
 	var deviceWidth = document.documentElement.clientWidth;
 	console.log(deviceWidth)
-		if(deviceWidth >= 375){
-			document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
-		}else{
-			document.documentElement.style.fontSize = deviceWidth / 26.66 + 'px';
-		}
 	
+		document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
+		
 	   }, false);
 	   
 	window.onresize = function(){
 		var deviceWidth = document.documentElement.clientWidth;
-		if(deviceWidth >= 375){
+		
 			document.documentElement.style.fontSize = deviceWidth / 31.25 + 'px';
-		}else{
-			document.documentElement.style.fontSize = deviceWidth / 26.66 + 'px';
-		}
+		
 	};
 })();
 
